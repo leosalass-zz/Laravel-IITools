9eec2b60f449ca45f454dc19be5b74b19b6cd413
Leonardo Salas <leosalass@gmail.com>
2018-08-17 03:02:56 -0500

    image upload correction

--- src/IITools.php
+++ src/IITools.php
@@ -59,8 +59,10 @@ class IITools extends Controller
             //if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
             if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
                 //echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+                return $target_file;
             } else {
                 //echo "Sorry, there was an error uploading your file.";
+                return null;
             }
         }
     }
