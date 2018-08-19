0899549346117636042c92a7611750ad66974848
Leonardo Salas <leosalass@gmail.com>
2018-08-19 12:15:59 -0500

    max file size limit taking care of php init

--- src/IITools.php
+++ src/IITools.php
@@ -13,55 +13,65 @@ class IITools extends Controller
     {
         self::directories($target_dir);
 
-        $target_file = self::$base_image_path . $target_dir . DIRECTORY_SEPARATOR . $file_name;
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-        // Check if image file is a actual image or fake image
-        $check = getimagesize($_FILES[$input_name]["tmp_name"]);
-        if ($check !== false) {
-            //echo "File is an image - " . $check["mime"] . ".";
+        try {
+            $target_file = self::$base_image_path . $target_dir . DIRECTORY_SEPARATOR . $file_name;
             $uploadOk = 1;
-        } else {
-            //echo "File is not an image.";
-            $uploadOk = 0;
-        }
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-        // Check if file already exists
-        if ($override) {
-            if (file_exists($target_file)) {
-                echo "Sorry, file already exists.";
-                $uploadOk = 0;
-            }
-        }
-        // Check file size
-        if ($file_limit_in_KB > 0) {
-            if ($_FILES["fileToUpload"]["size"] > 50000000) {
-                echo "Sorry, your file is too large.";
-                $uploadOk = 0;
-            }
-        }
-
-        // Allow certain file formats
-        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-            && $imageFileType != "gif") {
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-            $uploadOk = 0;
-        }
-
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
-            // if everything is ok, try to upload file
-        } else {
-            //if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                //echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
-                return $target_file;
+            // Check if image file is a actual image or fake image
+            $check = getimagesize($_FILES[$input_name]["tmp_name"]);
+            if ($check !== false) {
+                //echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
             } else {
-                //echo "Sorry, there was an error uploading your file.";
-                return null;
+                //echo "File is not an image.";
+                $uploadOk = 0;
             }
+
+            // Check if file already exists
+            if ($override) {
+                if (file_exists($target_file)) {
+                    echo "Sorry, file already exists.";
+                    $uploadOk = 0;
+                }
+            }
+            // Check file size
+            if ($file_limit_in_KB > 0) {
+                if ($_FILES["fileToUpload"]["size"] > self::file_upload_max_size()
+                    || $_FILES["fileToUpload"]["size"] > $file_limit_in_KB) {
+                    echo "Sorry, your file is too large.";
+                    $uploadOk = 0;
+                }
+            } else {
+                if ($_FILES["fileToUpload"]["size"] > self::file_upload_max_size()) {
+                    echo "Sorry, your file is too large.";
+                    $uploadOk = 0;
+                }
+            }
+
+            // Allow certain file formats
+            if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif") {
+                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                $uploadOk = 0;
+            }
+
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                echo "Sorry, your file was not uploaded.";
+                // if everything is ok, try to upload file
+            } else {
+                //if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                    //echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+                    return $target_file;
+                } else {
+                    //echo "Sorry, there was an error uploading your file.";
+                    return null;
+                }
+            }
+        } catch (\Exception $e) {
+            return null;
         }
     }
 
@@ -460,6 +470,41 @@ class IITools extends Controller
         } catch (\Exception $e) {
             /** TODO: check why the exception is not catching errors */
             file_put_contents($fichero, $e->getMessage());
+        }
+    }
+
+    // Returns a file size limit in bytes based on the PHP upload_max_filesize
+    // and post_max_size
+    public static function file_upload_max_size()
+    {
+        static $max_size = -1;
+
+        if ($max_size < 0) {
+            // Start with post_max_size.
+            $post_max_size = self::parse_size(ini_get('post_max_size'));
+            if ($post_max_size > 0) {
+                $max_size = $post_max_size;
+            }
+
+            // If upload_max_size is less, then reduce. Except if upload_max_size is
+            // zero, which indicates no limit.
+            $upload_max = self::parse_size(ini_get('upload_max_filesize'));
+            if ($upload_max > 0 && $upload_max < $max_size) {
+                $max_size = $upload_max;
+            }
+        }
+        return $max_size;
+    }
+
+    public static function parse_size($size)
+    {
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+        $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+        if ($unit) {
+            // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+        } else {
+            return round($size);
         }
     }
 }
